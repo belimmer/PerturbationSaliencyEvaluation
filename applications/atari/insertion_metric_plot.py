fixed minor error in AUC calculation (resulting in minor changes in the insertion metric results for Atari) and uploaded final graphs

applications/atari/insertion_metric_plot.py
@@ -2,7 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-
+# Normalizing formula from https://arxihttps://arxiv.org/pdf/2001.00396.pdfv.org/pdf/2001.00396.pdf
+# did not use this sicne we wanted to show the confidence range
 def normalize(arr):
     out = []
     b = arr[0]
@@ -13,13 +14,33 @@ def normalize(arr):
     return out
 
 
-#approaches = ["Occlusion Sensitivity Original", "Occlusion Sensitivity Modified", "Noise Sensitiviy Black", "Noise Sensitiviy Blur", "LIME", "RISE", "Random"]
 approaches = ["Occlusion Sensitivity Original", "Noise Sensitiviy Black", "Noise Sensitiviy Blur", "LIME", "RISE", "Random"]
-# color = ["y","c","r","k","g", "b"] better to use sns colorblind palette
-ignored_index = [1]
+# color = ["y","c","r","k","g", "b"] better to use sns colorblind
+# In case you one only wants to look at certain approaches
+ignored_index = [5]
 
-env = "space_invaders"
+env = "perdictions"
 
+
+# needs to be defined here since we need approaches
+def auc(data):
+    """
+    simple Area Under the curve calculation using sum_of_values/number_of_values
+    :param data:
+    :return:
+    """
+    i = 0
+    for arr in data:
+        if i in ignored_index:
+            i += 1
+            continue
+        print(approaches[i])
+        auc = arr.sum() / (arr.shape[0])
+        print(auc)
+        i += 1
+
+
+# load the different data checkpoints
 x_0 = np.load(file="figures/backup_" + env + "/pred_100.npy")
 x_1 = np.load(file="figures/backup_" + env + "/pred_200.npy")
 x_2 = np.load(file="figures/backup_" + env + "/pred_300.npy")
@@ -31,6 +52,8 @@ x_7 = np.load(file="figures/backup_" + env + "/pred_800.npy")
 x_8 = np.load(file="figures/backup_" + env + "/pred_900.npy")
 x_9 = np.load(file="figures/backup_" + env + "/pred_1000.npy")
 
+# transpose the data arrays such that the first dimension corrsponds to the approaches (e.g. x_0[0] is the data for
+# Occlusion Sensitivity
 x_0 = np.transpose(x_0, (1,2,0))
 x_1 = np.transpose(x_1, (1,2,0))
 x_2 = np.transpose(x_2, (1,2,0))
@@ -42,6 +65,7 @@ x_7 = np.transpose(x_7, (1,2,0))
 x_8 = np.transpose(x_8, (1,2,0))
 x_9 = np.transpose(x_9, (1,2,0))
 
+# combine the data checkpoints to one array
 x = np.concatenate((x_0, x_1), axis=2)
 x = np.concatenate((x, x_2), axis=2)
 x = np.concatenate((x, x_3), axis=2)
@@ -51,32 +75,26 @@ x = np.concatenate((x, x_6), axis=2)
 x = np.concatenate((x, x_7), axis=2)
 x = np.concatenate((x, x_8), axis=2)
 x = np.concatenate((x, x_9), axis=2)
+# average the values for each step of the insertion game over all tested inputs
 number = x.shape[2]
 x = np.sum(x, axis=2)
 x = x / number
 
-# needs to be defined here since we need approaches
-def auc(data):
-    i = 0
-    for arr in data:
-        if i in ignored_index:
-            i += 1
-            continue
-        print(approaches[i])
-        auc = arr.sum() / (arr.shape[0])
-        print(auc)
-        i += 1
-
+# AUC for raw softmax values
 auc(x)
 
+# normalize prediction values to have minimum 0 and maximum 1
 x_normalized = []
 for appr in x:
     x_normalized.append(normalize(appr))
 
+# AUC for normalized values
 print("normalized Auc")
 auc(x_normalized)
 
-# to generate graph without normalization iterate over x
+
+# Plot settings
+# to generate graph with normalization iterate over x_normalized
 data = x
 sns.set(palette='colorblind', style="whitegrid")
 #plt.figure(figsize=(10, 7))